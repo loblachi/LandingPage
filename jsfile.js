bae135ee9f8afef8f83aaf5e9a993ea18f398321
Trevlin <152470186+loblachi@users.noreply.github.com>
2024-03-04 16:00:20 +0200

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+		$(".ham").on("click", function(){
+			console.log("open mf"); 
+			$(".close").css({
+				'display':'inline-block'
+			})
+			$(".ham").css({
+				'display':'none'
+			})
+			$(".navUL").addClass("navDropDown"); 
+
+			 $(".div-ham-close").attr("aria-expanded" , "true") 
+	    })
+
+	    $(".close").on("click", function(){   
+			console.log("time to close"); 
+			$(".ham").css({
+				'display':'inline-block'
+			})
+			$(".close").css({
+				'display':'none'
+			})
+			$(".navUL").removeClass("navDropDown");
+
+			$(".div-ham-close").attr("aria-expanded" , "false")
+	    })
+
+})
+
+/*  */
